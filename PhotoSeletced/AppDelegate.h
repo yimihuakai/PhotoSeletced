c7c232f25459b592aea955834b074be197e18a97
Zhiyun <zhiyunguo@hengtiansoft.com>
2016-02-04 16:32:03 +0800

    Initial Commit

---
+++ PhotoSeletced/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoSeletced
+//
+//  Created by Zhiyun on 16/2/4.
+//  Copyright © 2016年 Zhiyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoSeletced/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoSeletced
+//
+//  Created by Zhiyun on 16/2/4.
+//  Copyright © 2016年 Zhiyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
