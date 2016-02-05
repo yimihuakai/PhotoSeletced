955b807957778c1ed7b09b771fffab1b0571c59a
Zhiyun <zhiyunguo@hengtiansoft.com>
2016-02-05 13:14:17 +0800

    init

---
+++ PhotoSeletced/PhotoCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.h
+//  PhotoSeletced
+//
+//  Created by Zhiyun on 16/2/4.
+//  Copyright © 2016年 Zhiyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ PhotoSeletced/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  PhotoSeletced
+//
+//  Created by Zhiyun on 16/2/4.
+//  Copyright © 2016年 Zhiyun. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
